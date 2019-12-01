939b6f0e78544c421146151b49388269bbc3935c
Peder Sørensen <pks@in-commodities.com>
2019-12-01 10:54:16 +0100

    Add day1 exercise

---
+++ AdventOfCode/Excercises2019.fsx
@@ -0,0 +1,34 @@
+﻿open System
+open System.IO
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+let readInput file = File.ReadAllLines("input2019/" + file + ".txt")
+
+module Day1 =
+  // Part 1
+  let getFuel mass = max 0 (mass / 3 - 2)
+  getFuel 12
+  getFuel 14
+  getFuel 1969
+  getFuel 100756
+
+  let totalFuel =
+    readInput "day1"
+    |> Array.sumBy(int >> getFuel)
+
+  // Part 2
+  let rec getFuel' mass =
+    let fuel = getFuel mass
+    if fuel > 0 then
+      fuel + getFuel' fuel
+    else fuel
+
+  getFuel' 12
+  getFuel' 14
+  getFuel' 1969
+  getFuel' 100756
+
+  let totalFuel' =
+    readInput "day1"
+    |> Array.sumBy(int >> getFuel')
