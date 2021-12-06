3e71eeca617798bfe53b4765b321f779ffe76fe6
Peder Sørensen <pks1986@gmail.com>
2021-12-06 21:07:16 +0100

    Solve day 2

--- AdventOfCode/Excercises2021.fsx
+++ AdventOfCode/Excercises2021.fsx
@@ -46,3 +46,53 @@ module Day1 =
     |> Array.windowed 3
     |> Array.map Array.sum
     |> countIncreases
+
+module Day2 =
+
+  let input = readInput 2
+
+  let sample = [|
+    "forward 5"
+    "down 5"
+    "forward 8"
+    "up 3"
+    "down 8"
+    "forward 2"
+  |]
+
+  let (|Up|Down|Forward|) (s: string) =
+    match s.Split(' ') with
+    | [| "up"      ; x |] -> Up     (int x)
+    | [| "down"    ; x |] -> Down   (int x)
+    | [| "forward" ; x |] -> Forward(int x)
+    | _ -> failwithf $"Unknown instruction: '{s}'"
+
+  let moveIt directions =
+    (struct(0, 0), directions)
+    ||> Array.fold(fun struct(h, d) s ->
+      match s with
+      | Up      x -> h, d - x
+      | Down    x -> h, d + x
+      | Forward x -> h + x, d
+    )
+    |> fun struct(h, d) -> h * d
+
+  // 2147104
+  let part1() =
+    moveIt input
+
+  let moveItAgain directions =
+    (struct(0, 0, 0), directions)
+    ||> Array.fold(fun struct(h, d, aim) s ->
+      match s with
+      | Up x      -> h, d, aim - int x
+      | Down x    -> h, d, aim + int x
+      | Forward x -> h + int x, d + aim * int x, aim
+    )
+    |> fun struct(h, d, _) -> h * d
+
+  // 2044620088
+  let part2() =
+    moveItAgain input
+
+
