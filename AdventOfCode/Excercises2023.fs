907593c4b1064520cb8eff4339a9ab55ff20379b
Peder Sørensen <pks1986@gmail.com>
2023-12-06 21:54:43 +0100

    Reverse day ordering
    It becomes more and more annoying to scroll to the bottom for each new
    day. With the latest day on top, it's much simpler.

--- AdventOfCode/Excercises2023.fs
+++ AdventOfCode/Excercises2023.fs
@@ -13,77 +13,76 @@ open System.Buffers
 open System.Text.RegularExpressions
 open System.Collections.Generic
 
-module Day01 =
+module Day03 =
 
-  let sample1 (result: int) = makeSample result [|
-    "1abc2"
-    "pqr3stu8vwx"
-    "a1b2c3d4e5f"
-    "treb7uchet"
+  let input = [|
+    "467..114.."
+    "...*......"
+    "..35..633."
+    "......#..."
+    "617*......"
+    ".....+.58."
+    "..592....."
+    "......755."
+    "...$.*...."
+    ".664.598.."
   |]
 
-  let digits = SearchValues.Create("0123456789")
+  let sample (result: int) = makeSample result input
 
-  [<Theory>]
-  [<FileData(2023, 1, 54_634)>]
-  [<MemberData(nameof sample1, 142)>]
-  let part1 (input: string []) expected =
+  let getNumbers input =
     input
-    |> Array.sumBy(fun line ->
-      let span = line.AsSpan()
-      let i = span.IndexOfAny(digits)
-      let j = span.LastIndexOfAny(digits)
-      10 * int(line[i] - '0') + int(line[j] - '0')
-    ) =! expected
-
-  let sample2 (result: int) = makeSample result [|
-    "two1nine"
-    "eightwothree"
-    "abcone2threexyz"
-    "xtwone3four"
-    "4nineeightseven2"
-    "zoneight234"
-    "7pqrstsixteen"
-  |]
-
-  let digitsAsWords =
-    [| "zero"; "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" |]
-
-  [<Theory>]
-  [<FileData(2023, 1, 53855)>]
-  [<MemberData(nameof sample2, 281)>]
-  let part2 (input: string []) expected =
-    input
-    |> Array.sumBy(fun line ->
-      let span = line.AsSpan()
-      let i = span.IndexOfAny(digits)
-      let j = span.LastIndexOfAny(digits)
-
-      let indexes =
-        digitsAsWords
-        |> Array.mapi(fun value digit -> value, line.IndexOf(digit), line.LastIndexOf(digit))
-
-      let minWordValue, i2, _ =
-        indexes
-        |> Array.minBy(fun (_, idx, _) ->
-          if idx < 0 then Int32.MaxValue else idx
-        )
-
-      let minValue =
-        if i < 0 || (-1 < i2 && i2 < i)
-        then minWordValue
-        else int(line[i] - '0')
-
-      let maxWordValue, _, j2 = indexes |> Array.maxBy(fun (_, _, idx) -> idx)
-
-      let maxValue =
-        if j < 0 || (-1 < j2 && j2 > j)
-        then maxWordValue
-        else int(line[j] - '0')
-
-      10 * minValue + maxValue
+    |> Array.mapi(fun i line -> [|
+        for m in Regex.Matches(line, "[0-9]+") do
+          let partNumberId = (i, m.Index)
+          let value = (Int32.Parse(m.ValueSpan), partNumberId)
+          for j = 0 to m.Length - 1 do
+            (i, m.Index + j), value
+      |]
     )
-    =! expected
+    |> Array.collect id
+    |> Map.ofArray
+
+  let allDirections = [|
+    (-1, -1); (-1, 0); (-1, 1)
+    ( 0, -1);          ( 0, 1)
+    ( 1, -1); ( 1, 0); ( 1, 1)
+  |]
+
+  [<Theory>]
+  [<FileData(2023, 3, 553079)>]
+  [<MemberData(nameof sample, 4361)>]
+  let part1 (input: string []) expected =
+    let numbers       = getNumbers input
+    let notPartNumber = SearchValues.Create("0123456789.")
+    let seen          = HashSet()
+    let mutable sum = 0
+    for i = 1 to input.Length - 2 do
+      for j = 1 to input.[i].Length - 2 do
+        if notPartNumber.Contains(input[i].[j]) |> not then
+          for (di, dj) in allDirections do
+            match numbers.TryFind(i + di, j + dj) with
+            | Some(value, partNumberId) when seen.Add(partNumberId) ->
+              sum <- sum + value
+            | _ -> ()
+    sum =! expected
+
+  [<Theory>]
+  [<FileData(2023, 3, 84363105)>]
+  [<MemberData(nameof sample, 467835)>]
+  let part2 (input: string []) expected =
+    let numbers     = getNumbers input
+    let mutable sum = 0
+    for i = 1 to input.Length - 2 do
+      for j = 1 to input.[i].Length - 2 do
+        if input[i].[j] = '*' then
+          allDirections
+          |> Array.choose(fun (di, dj) -> numbers.TryFind(i + di, j + dj))
+          |> Array.distinctBy snd
+          |> function
+          | [| (v1, _); (v2, _) |] -> sum <- sum + v1 * v2
+          | _ -> ()
+    sum =! expected
 
 module Day02 =
 
@@ -155,76 +154,77 @@ module Day02 =
       |> Seq.fold(fun prod kvp -> prod * kvp.Value) 1
     ) =! expected
 
-module Day03 =
+module Day01 =
 
-  let input = [|
-    "467..114.."
-    "...*......"
-    "..35..633."
-    "......#..."
-    "617*......"
-    ".....+.58."
-    "..592....."
-    "......755."
-    "...$.*...."
-    ".664.598.."
+  let sample1 (result: int) = makeSample result [|
+    "1abc2"
+    "pqr3stu8vwx"
+    "a1b2c3d4e5f"
+    "treb7uchet"
   |]
 
-  let sample (result: int) = makeSample result input
-
-  let getNumbers input =
-    input
-    |> Array.mapi(fun i line -> [|
-        for m in Regex.Matches(line, "[0-9]+") do
-          let partNumberId = (i, m.Index)
-          let value = (Int32.Parse(m.ValueSpan), partNumberId)
-          for j = 0 to m.Length - 1 do
-            (i, m.Index + j), value
-      |]
-    )
-    |> Array.collect id
-    |> Map.ofArray
-
-  let allDirections = [|
-    (-1, -1); (-1, 0); (-1, 1)
-    ( 0, -1);          ( 0, 1)
-    ( 1, -1); ( 1, 0); ( 1, 1)
-  |]
+  let digits = SearchValues.Create("0123456789")
 
   [<Theory>]
-  [<FileData(2023, 3, 553079)>]
-  [<MemberData(nameof sample, 4361)>]
+  [<FileData(2023, 1, 54_634)>]
+  [<MemberData(nameof sample1, 142)>]
   let part1 (input: string []) expected =
-    let numbers       = getNumbers input
-    let notPartNumber = SearchValues.Create("0123456789.")
-    let seen          = HashSet()
-    let mutable sum = 0
-    for i = 1 to input.Length - 2 do
-      for j = 1 to input.[i].Length - 2 do
-        if notPartNumber.Contains(input[i].[j]) |> not then
-          for (di, dj) in allDirections do
-            match numbers.TryFind(i + di, j + dj) with
-            | Some(value, partNumberId) when seen.Add(partNumberId) ->
-              sum <- sum + value
-            | _ -> ()
-    sum =! expected
+    input
+    |> Array.sumBy(fun line ->
+      let span = line.AsSpan()
+      let i = span.IndexOfAny(digits)
+      let j = span.LastIndexOfAny(digits)
+      10 * int(line[i] - '0') + int(line[j] - '0')
+    ) =! expected
+
+  let sample2 (result: int) = makeSample result [|
+    "two1nine"
+    "eightwothree"
+    "abcone2threexyz"
+    "xtwone3four"
+    "4nineeightseven2"
+    "zoneight234"
+    "7pqrstsixteen"
+  |]
+
+  let digitsAsWords =
+    [| "zero"; "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" |]
 
   [<Theory>]
-  [<FileData(2023, 3, 84363105)>]
-  [<MemberData(nameof sample, 467835)>]
+  [<FileData(2023, 1, 53855)>]
+  [<MemberData(nameof sample2, 281)>]
   let part2 (input: string []) expected =
-    let numbers     = getNumbers input
-    let mutable sum = 0
-    for i = 1 to input.Length - 2 do
-      for j = 1 to input.[i].Length - 2 do
-        if input[i].[j] = '*' then
-          allDirections
-          |> Array.choose(fun (di, dj) -> numbers.TryFind(i + di, j + dj))
-          |> Array.distinctBy snd
-          |> function
-          | [| (v1, _); (v2, _) |] -> sum <- sum + v1 * v2
-          | _ -> ()
-    sum =! expected
+    input
+    |> Array.sumBy(fun line ->
+      let span = line.AsSpan()
+      let i = span.IndexOfAny(digits)
+      let j = span.LastIndexOfAny(digits)
+
+      let indexes =
+        digitsAsWords
+        |> Array.mapi(fun value digit -> value, line.IndexOf(digit), line.LastIndexOf(digit))
+
+      let minWordValue, i2, _ =
+        indexes
+        |> Array.minBy(fun (_, idx, _) ->
+          if idx < 0 then Int32.MaxValue else idx
+        )
+
+      let minValue =
+        if i < 0 || (-1 < i2 && i2 < i)
+        then minWordValue
+        else int(line[i] - '0')
+
+      let maxWordValue, _, j2 = indexes |> Array.maxBy(fun (_, _, idx) -> idx)
+
+      let maxValue =
+        if j < 0 || (-1 < j2 && j2 > j)
+        then maxWordValue
+        else int(line[j] - '0')
+
+      10 * minValue + maxValue
+    )
+    =! expected
 
 #if INTERACTIVE
 
