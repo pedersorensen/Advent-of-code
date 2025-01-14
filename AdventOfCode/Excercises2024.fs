2a72348c5fe58a7267e2f32268d8d9c5613f0b82
Peder Sørensen <pks1986@gmail.com>
2025-01-14 22:12:40 +0100

    Solve day 24, part 1

--- AdventOfCode/Excercises2024.fs
+++ AdventOfCode/Excercises2024.fs
@@ -17,6 +17,128 @@ open System.Runtime.InteropServices
 makeTemplate 2024 25 |> clip
 #endif
 
+module Day24 =
+
+  let input = [|
+    "x00: 1"
+    "x01: 1"
+    "x02: 1"
+    "y00: 0"
+    "y01: 1"
+    "y02: 0"
+    ""
+    "x00 AND y00 -> z00"
+    "x01 XOR y01 -> z01"
+    "x02 OR y02 -> z02"
+  |]
+
+  let input2 = [|
+    "x00: 1"
+    "x01: 0"
+    "x02: 1"
+    "x03: 1"
+    "x04: 0"
+    "y00: 1"
+    "y01: 1"
+    "y02: 1"
+    "y03: 1"
+    "y04: 1"
+    ""
+    "ntg XOR fgs -> mjb"
+    "y02 OR x01 -> tnw"
+    "kwq OR kpj -> z05"
+    "x00 OR x03 -> fst"
+    "tgd XOR rvg -> z01"
+    "vdt OR tnw -> bfw"
+    "bfw AND frj -> z10"
+    "ffh OR nrd -> bqk"
+    "y00 AND y03 -> djm"
+    "y03 OR y00 -> psh"
+    "bqk OR frj -> z08"
+    "tnw OR fst -> frj"
+    "gnj AND tgd -> z11"
+    "bfw XOR mjb -> z00"
+    "x03 OR x00 -> vdt"
+    "gnj AND wpb -> z02"
+    "x04 AND y00 -> kjc"
+    "djm OR pbm -> qhw"
+    "nrd AND vdt -> hwm"
+    "kjc AND fst -> rvg"
+    "y04 OR y02 -> fgs"
+    "y01 AND x02 -> pbm"
+    "ntg OR kjc -> kwq"
+    "psh XOR fgs -> tgd"
+    "qhw XOR tgd -> z09"
+    "pbm OR djm -> kpj"
+    "x03 XOR y03 -> ffh"
+    "x00 XOR y04 -> ntg"
+    "bfw OR bqk -> z06"
+    "nrd XOR fgs -> wpb"
+    "frj XOR qhw -> z04"
+    "bqk OR frj -> z07"
+    "y03 OR x01 -> nrd"
+    "hwm AND bqk -> z03"
+    "tgd XOR rvg -> z12"
+    "tnw OR pbm -> gnj"
+  |]
+
+  type Operation =
+  | And
+  | Or
+  | Xor
+
+  let parse (input: string array) =
+    let initialValues, connections = Dictionary(), SortedDictionary()
+    for line in input do
+      match line.Split(':', StringSplitOptions.TrimEntries) with
+      | [| |]
+      | [| "" |] -> ()
+      | [| name ; value |] -> initialValues.Add(name, int value)
+      | _ ->
+        match line.Split(' ', StringSplitOptions.TrimEntries) with
+        | [| a ; op ; b ; "->" ; c |] ->
+          let op =
+            match op with
+            | "AND" -> (&&&)
+            | "OR"  -> (|||)
+            | "XOR" -> (^^^)
+            | _ -> failwith "Invalid operation"
+          connections.Add(c, (a, op, b))
+        | l -> failwithf "Invalid input: %A" l
+    initialValues, connections
+
+  let sample (result: int) = makeSample result input
+  let sample2 (result: int) = makeSample result input2
+
+  [<Theory>]
+  [<FileData(2024, 24, 57270694330992L)>]
+  [<MemberData(nameof sample, 4)>]
+  [<MemberData(nameof sample2, 2024)>]
+  let part1 (input: string array) expected =
+    let values, connections = parse input
+    let rec getValue name =
+      match values.TryGetValue(name) with
+      | true, value -> value
+      | _ ->
+        let (a, (++), b) = connections[name]
+        let value = getValue a ++ getValue b
+        values.Add(name, value)
+        value
+    ((0L, 1L), connections)
+    ||> Seq.fold(fun (sum, powerOfTwo) kvp ->
+      if kvp.Key.StartsWith("z")
+      then sum + powerOfTwo * int64(getValue kvp.Key), 2L * powerOfTwo
+      else sum, powerOfTwo
+    )
+    |> fst
+    =! expected
+
+  [<Theory>]
+  [<FileData(2024, 24, 0)>]
+  [<MemberData(nameof sample, 0)>]
+  let part2 (input: string array) expected =
+    -1 =! expected
+
 module Day22 =
 
   let input = [|
